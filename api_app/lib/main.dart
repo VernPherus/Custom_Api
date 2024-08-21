b5522a5f111a94783f1889e70638c20987c34ded
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2024-08-21 10:23:31 +0800

    initial commit

---
+++ api_app/lib/main.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+void main(){
+  Index();
+}
+
+class Index extends StatelessWidget {
+  const Index({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
