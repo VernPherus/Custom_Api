8a7fda81a26cae755f83209b5368ac2ac9b85029
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2024-09-05 11:54:53 +0800

    reverted changes

--- api_app/lib/models/student.dart
+++ api_app/lib/models/student.dart
@@ -1,11 +1,10 @@
 class Student {
-  final String id;
+  final int id;
   final String firstName;
   final String lastName;
-  final String course;
+  final String course; 
   final String year;
   final bool enrolled;
-  final int versionKey;
 
   Student({
     required this.id,
@@ -13,16 +12,16 @@ class Student {
     required this.lastName,
     required this.course,
     required this.year,
-    required this.enrolled,
-    required this.versionKey,
+    required this.enrolled
   });
 
-  factory Student.fromJson(Map<String, dynamic> json) => Student(
-      id: json["_id"],
-      firstName: json["fname"],
-      lastName: json["lname"],
-      course: json["course"],
-      year: json["year"],
-      enrolled: json["enrolled"] == 0 ? false : true,
-      versionKey: json["__v"]);
+  factory Student.fromSqlfliteDB(Map<String, dynamic> map) => Student(
+    id: map["id"], 
+    firstName: map["firstName"] ?? '', 
+    lastName: map["lastName"] ?? '', 
+    course: map["course"] ?? '', 
+    year: map["year"] ?? '', 
+    enrolled: map["bool"] == 0 ? false : true
+  );
+
 }
--- api_app/lib/screens/student_view.dart
+++ api_app/lib/screens/student_view.dart
@@ -1,15 +1,111 @@
+<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 class StudentView extends StatefulWidget {
   const StudentView({super.key});
+=======
+import 'package:api_app/database/students_db.dart';
+import 'package:api_app/models/student.dart';
+import 'package:api_app/widgets/student_create_update.dart';
+import 'package:flutter/material.dart';
+
+class StudentView extends StatefulWidget {
+  final int studentID;
+
+  const StudentView({super.key, required this.studentID});
+>>>>>>> parent of bc7fec6 (restructured model, removed database and unused imports)
 
   @override
   State<StudentView> createState() => _StudentViewState();
 }
 
 class _StudentViewState extends State<StudentView> {
+<<<<<<< HEAD
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
+=======
+  Future<Student>? futureStudent;
+  final studentDB = StudentsDB();
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchStudentData(widget.studentID);
+  }
+
+  void fetchStudentData(int studentID) {
+    setState(() {
+      futureStudent = studentDB.fetchById(studentID);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Student Details'),
+        ),
+        body: FutureBuilder(
+            future: futureStudent,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                final student = snapshot.data!;
+
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(student.firstName),
+                          Text(student.lastName)
+                        ],
+                      ),
+                      Text(student.course),
+                      Text(student.year),
+                      Text(student.enrolled.toString()),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          OutlinedButton(
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  barrierDismissible: false,
+                                  builder: (_) => Create_Update_Student(
+                                      student: student,
+                                      onSubmit: (studentUp) async {
+                                        await studentDB.update(
+                                            id: student.id,
+                                            firstName: studentUp['firstName'],
+                                            lastName: studentUp['lastName'],
+                                            course: studentUp['course'],
+                                            year: studentUp['year'],
+                                            enrolled: studentUp['enrolled']);
+                                        fetchStudentData(widget.studentID);
+                                        if (!mounted) return;
+                                        Navigator.of(context).pop();
+                                      }));
+                            },
+                            child: const Text('Update'),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }
+            }));
+  }
+}
+>>>>>>> parent of bc7fec6 (restructured model, removed database and unused imports)
