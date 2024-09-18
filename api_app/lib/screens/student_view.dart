71a67f747a6ec3af7c95149e028773e22266db5c
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2024-09-18 14:04:08 +0800

    added new widgets

--- api_app/lib/screens/student_view.dart
+++ api_app/lib/screens/student_view.dart
@@ -1,6 +1,7 @@
 import 'package:api_app/api/api_service.dart';
 import 'package:api_app/models/student.dart';
 import 'package:api_app/widgets/student_form.dart';
+import 'package:api_app/widgets/student_view_widgets/title_card.dart';
 import 'package:flutter/material.dart';
 
 class StudentView extends StatefulWidget {
@@ -47,60 +48,70 @@ class _StudentViewState extends State<StudentView> {
             final student = snapshot.data!;
 
             return Padding(
-              padding: EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(0.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Name: ${student.firstName} ${student.lastName}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 25),
-                      ),
-                      Text(
-                        'Course: ${student.course}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 25),
-                      ),
-                      Text(
-                        'Year: ${student.year}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 25),
-                      ),
-                      Text(
-                        'Is Enrolled: ${student.enrolled}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 25),
-                      ),
-                    ],
+                  Flexible(
+                    flex: 1,
+                    fit: FlexFit.tight,
+                    child: TitleCard(
+                      titleText: '${student.firstName} ${student.lastName}',
+                    ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      OutlinedButton(
-                          onPressed: () {
-                            showDialog(
-                                context: context,
-                                builder: (_) => StudentForm(
-                                    student: student,
-                                    onSubmit: (updt_student) async {
-                                      await api_service.updateStudent(
-                                          id: student.id,
-                                          fname: updt_student['fname'],
-                                          lname: updt_student['lname'],
-                                          course: updt_student['course'],
-                                          year: updt_student['year'],
-                                          enrolled: updt_student['enrolled']);
-                                      fetchStudent(widget.id);
-                                      if (!mounted) return;
-                                      Navigator.of(context).pop();
-                                    }));
-                          },
-                          child: const Text("Update"))
-                    ],
-                  )
+                  Flexible(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Course: ${student.course}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                        Text(
+                          'Year: ${student.year}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                        Text(
+                          'Is Enrolled: ${student.enrolled}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: OutlinedButton(
+                                onPressed: () {
+                                  showDialog(
+                                      context: context,
+                                      builder: (_) => StudentForm(
+                                          student: student,
+                                          onSubmit: (updt_student) async {
+                                            await api_service.updateStudent(
+                                                id: student.id,
+                                                fname: updt_student['fname'],
+                                                lname: updt_student['lname'],
+                                                course: updt_student['course'],
+                                                year: updt_student['year'],
+                                                enrolled:
+                                                    updt_student['enrolled']);
+                                            fetchStudent(widget.id);
+                                            if (!mounted) return;
+                                            Navigator.of(context).pop();
+                                          }));
+                                },
+                                child: const Text("Update")),
+                          )
+                        ],
+                      ))
                 ],
               ),
             );
---
+++ api_app/lib/widgets/student_view_widgets/title_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class TitleCard extends StatelessWidget {
+  final String titleText;
+  const TitleCard({super.key, required this.titleText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(0.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(14.0),
+          bottomRight: Radius.circular(14.0),
+        ),
+      ),
+      color: Colors.grey,
+      elevation: 0,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ConstrainedBox(
+            constraints: BoxConstraints(maxHeight: 200),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    titleText,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  Divider(
+                    thickness: 3,
+                    color: Colors.black,
+                    height: 40,
+                  )
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
